7b8ce1b8c48ce3e6bd7cfe3ebcde6c0989901208
root <root@LAPTOP-RJL1JAA8.localdomain>
2026-03-24 19:00:49 +0700

    remove gitignore

---
+++ src/cmake/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/main"
+  "../bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/robot.cpp.o"
+  "CMakeFiles/main.dir/src/robot.cpp.o.d"
+  "CMakeFiles/main.dir/src/robotair.cpp.o"
+  "CMakeFiles/main.dir/src/robotair.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
